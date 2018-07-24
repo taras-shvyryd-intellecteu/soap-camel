323c16575bf6080dfdee7788b868702c5a26cd98
Taras <taras.shvyryd@intellecteu.com>
2018-07-24 11:55:11 +0300

    Initial commit

---
+++ src/main/java/my/demo/SoapClientRoute.java
@@ -0,0 +1,44 @@
+package my.demo;
+
+import my.demo.tempuri.Multiply;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.CxfEndpoint;
+import org.apache.camel.component.cxf.DataFormat;
+import org.apache.camel.dataformat.soap.SoapJaxbDataFormat;
+
+/**
+ * Created on 19.07.18 by taras
+ */
+public class SoapClientRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        CamelContext camelContext = getContext();
+
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setAddress("http://www.dneonline.com/calculator.asmx");
+        cxfEndpoint.setCamelContext(camelContext);
+        cxfEndpoint.setWsdlURL("calculator.wsdl");
+        cxfEndpoint.setDataFormat(DataFormat.RAW);
+
+        try {
+            cxfEndpoint.setServiceClass("my.demo.tempuri.Calculator");
+            camelContext.addEndpoint("calculator", cxfEndpoint);
+        } catch (Exception e) { e.printStackTrace(); }
+
+        Multiply mul = new Multiply();
+        mul.setIntA(5);
+        mul.setIntB(18);
+        SoapJaxbDataFormat soapDF = new SoapJaxbDataFormat("my.demo.tempuri");
+
+        from("timer:foo?period=60000&repeatCount=1")
+                .process(exchange -> exchange.getIn().setBody(mul))
+                .marshal(soapDF)
+                .log("${body}")
+                .setHeader("Content-Type", constant("text/xml"))
+                .to("calculator")
+                .unmarshal(soapDF)
+                .log("Result: ${body.getMultiplyResult()}");
+    }
+}
---
+++ src/main/java/my/demo/tempuri/MultiplyResponse.java
@@ -0,0 +1,52 @@
+
+package my.demo.tempuri;
+
+import javax.xml.bind.annotation.*;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="MultiplyResult" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "multiplyResult"
+})
+@XmlRootElement(name = "MultiplyResponse")
+public class MultiplyResponse {
+
+    @XmlElement(name = "MultiplyResult")
+    protected int multiplyResult;
+
+    /**
+     * Gets the value of the multiplyResult property.
+     * 
+     */
+    public int getMultiplyResult() {
+        return multiplyResult;
+    }
+
+    /**
+     * Sets the value of the multiplyResult property.
+     * 
+     */
+    public void setMultiplyResult(int value) {
+        this.multiplyResult = value;
+    }
+
+}
